7d9228adb0ac08ebf609b21ad4a11b0f7adba414
AlX33a <alexau123456789@gmail.com>
2024-04-14 01:34:55 +0300

    upd

--- start.sh
+++ start.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
-apt -y update
+apt-get update -y
 apt-get -y install git
 
-# docker + docker compose
+# docker
 apt -y install curl software-properties-common ca-certificates apt-transport-https
-wget -O- https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | sudo tee /etc/apt/keyrings/docker.gpg > /dev/null
-echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable"| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get -y install docker-ce
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# docker compose
 git clone https://github.com/docker/compose.git
 curl -L "https://github.com/docker/compose/releases/download/v2.19.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
@@ -19,16 +27,16 @@ apt -y install python3.11-venv
 update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
 
 # nodejs + npm
-curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash - 
+curl -sL https://deb.nodesource.com/setup_20.x | -E bash - 
 apt-get install nodejs -y
 apt -y update
 apt autoremove -y
 
 # update + upgrade
 set -e
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
+apt-get update -y
+apt-get upgrade -y
+apt-get dist-upgrade -y
 
 # versions
 docker -v
