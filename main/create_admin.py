60794c41351125cf79a5e6346b5248ed335bd674
AlX33a <alexau123456789@gmail.com>
2024-04-14 02:33:52 +0300

    nm

--- main/create_admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth import get_user_model
-import os
-
-User = get_user_model()
-User.objects.create_superuser(os.environ["DJANGO_SUPERUSER_PASSWORD"], 'admin@ad.min', os.environ["DJANGO_SUPERUSER_USERNAME"])
--- main/docker-entrypoint.sh
+++ main/docker-entrypoint.sh
@@ -6,6 +6,9 @@ python3 manage.py migrate
 
 if [ "$ENVIRONMENT" = "DEV" ]; then
 
+echo "Creating superuser from compose ENV vars"
+python3 manage.py createsuperuser --noinput --email admin@ad.min
+
 echo "Starting development server"
 python3 manage.py runserver 0.0.0.0:8000
 
