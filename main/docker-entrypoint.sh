7ec2838ea34e42be676cd81127c59613b838d43b
AlX33a <alexau123456789@gmail.com>
2024-04-13 23:56:27 +0300

    init

---
+++ main/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+echo "Making migrations"
+python3 manage.py makemigrations
+
+echo "Applying migrations"
+python3 manage.py migrate
+
+if [ "$ENVIRONMENT" = "DEV" ]; then
+
+echo "Starting development server"
+python3 manage.py runserver 0.0.0.0:8000
+
+echo "Creating superuser from compose ENV vars"
+python3 manage.py createsuperuser --noinput --email admin@ad.min
+
+elif [ "$ENVIRONMENT" = "PROD" ]; then
+
+echo "Collecting static files"
+python3 manage.py collectstatic --noinput
+
+echo "Running gunicorn"
+gunicorn --bind 0.0.0.0:8000 main.wsgi:application --workers  16
+
+fi
