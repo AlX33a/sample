4e073661e7217c1b67dba2daba828941839be803
AlX33a <alexau123456789@gmail.com>
2024-04-14 21:13:23 +0300

    ddd

--- main/docker-entrypoint.sh
+++ main/docker-entrypoint.sh
@@ -21,6 +21,6 @@ echo "Collecting static files"
 python3 manage.py collectstatic --noinput
 
 echo "Running gunicorn"
-gunicorn --bind 0.0.0.0:8000 main.wsgi:application --timeout 1000 --workers 16
+gunicorn --bind 0.0.0.0:8000 main.wsgi:application --timeout 1000 --workers 4
 
 fi
