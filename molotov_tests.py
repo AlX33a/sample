db376c09309bf0126fe88253eea46c3b51e52f26
AlX33a <alexau123456789@gmail.com>
2024-04-17 22:45:39 +0300

    anti ddos

--- molotov_tests.py
+++ molotov_tests.py
@@ -8,13 +8,13 @@ BASE_URL = "http://188.225.78.8"
 
 @molotov.scenario(weight=50)
 async def scenario_one(session):
-    await asyncio.sleep(random.uniform(0.5, 1.5))
+    await asyncio.sleep(random.uniform(0.5, 2))
     async with session.get(BASE_URL) as resp:
         assert resp.status == 200
 
 @molotov.scenario(weight=50)
 async def scenario_two(session):
-    await asyncio.sleep(random.uniform(0.5, 1.5))
+    await asyncio.sleep(random.uniform(0.5, 2))
     async with session.post(API_URL) as resp:
         assert resp.status == 200
 
