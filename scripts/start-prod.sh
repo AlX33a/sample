270a5540a5b5bec7811c7ece828ba254448de7a8
AlX33a <alexau123456789@gmail.com>
2024-05-31 02:08:15 +0300

    mm

--- scripts/start-prod.sh
+++ scripts/start-prod.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 echo ================================ START ================================
-
+# Add Docker's official GPG key
 sudo apt-get update -y
-sudo apt-get install git curl software-properties-common ca-certificates apt-transport-https -y
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# docker & docker compose
-sudo apt update -y
-sudo apt-get install docker docker-compose -y
-
-# update
-sudo apt-get update -y
-sudo apt autoremove -y
+# Add the repository to Apt sources
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # docker mirror timeweb
 sudo echo '{ "registry-mirrors" : [ "https://dockerhub.timeweb.cloud" ] }' > /etc/docker/daemon.json
 head -n 1 /etc/docker/daemon.json > /etc/docker/daemon.json
 sudo systemctl reload docker
 
-# add env vars
-# eth0 ip set in containers env var
+# add env vars (eth0 ip set in containers env var)
 for file in ../scripts/envs/.env*; do echo "IP = $(ip a s eth0 | awk '/inet / {print$2}' | cut -f1 -d\/)" » "$file"; done
 
 # # python
